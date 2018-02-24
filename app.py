7d8e1e8ac47289ec57459bc646cddf6508e76faa
Zuhaib Ahmad <xuhaib_ahmad@yahoo.com>
2018-02-25 00:17:10 +0500

    Back to vanilla Bootstrap

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask
 from flask.templating import render_template
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html', title="Home")
 
 
 if __name__ == '__main__':
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run(debug=True)
